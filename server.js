7f002219bcbce1c3fefce6f65b34ac18c8f17384
Rudro Khan <rudrokhan.pro@gmail.com>
2020-05-06 01:54:45 +0200

    MongoDB connection

--- server.js
+++ server.js
@@ -2,6 +2,23 @@ const express = require("express");
 
 const server = express();
 
+// MongoDB connection
+const mongoose = require("mongoose");
+const MONGODB_URL = "mongodb://localhost:27017";
+mongoose.connect(MONGODB_URL, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("error", () =>
+  console.log("Connection failed  to MongoDB server"),
+);
+
+mongoose.connection.once("open", () =>
+  console.log("Connected to MongoDB server"),
+);
+
 // Middlewares and plugins
 const compression = require("compression");
 const cors = require("cors");
