2d731e9eb493b42c954e1cbdf2ae56fe80e1bafa
Rudro Khan <rudrokhan.pro@gmail.com>
2020-05-07 15:15:09 +0200

    HTTP Status codes

---
+++ utils/status_codes.js
@@ -0,0 +1,13 @@
+/**
+ * HTTP Status Codes
+ */
+const STATUS = {
+  OK: 200,
+  CREATED: 201,
+  SUCCESS: 202,
+  BAD_REQUEST: 400,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+};
+
+module.exports = STATUS;
